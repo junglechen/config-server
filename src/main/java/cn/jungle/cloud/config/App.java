cd0eabfcf55b90d929ca49ddea739b6382dfc638
junglechen@163.com <>
2018-01-24 17:22:15 +0800

    Signed-off-by: junglechen@163.com <>

---
+++ src/main/java/cn/jungle/cloud/config/App.java
@@ -0,0 +1,22 @@
+package cn.jungle.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+        System.out.println( "Hello World!" );
+    }
+}
